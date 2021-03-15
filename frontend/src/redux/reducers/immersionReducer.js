601aedf50662d86b5161c44ca55b409537b90664
Eloy Galan <gfeb29@gmail.com>
2021-03-15 15:09:17 +0100

    Add immersionReducer test

--- frontend/src/redux/reducers/immersionReducer.js
+++ frontend/src/redux/reducers/immersionReducer.js
@@ -4,8 +4,6 @@ import initialState from '../store/initialState';
 function immersionReducer(state = { immersionHistory: initialState.immersionHistory }, action) {
   switch (action.type) {
     case actionTypes.LOAD_IMMERSIONS:
-      // eslint-disable-next-line no-debugger
-      debugger;
       return { ...state, immersionHistory: action.immersions };
     default:
       return state;
