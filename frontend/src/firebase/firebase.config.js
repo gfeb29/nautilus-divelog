82fd3608cf5fbe133a5a09e0d5a9f7d15898f59d
Eloy Galan <gfeb29@gmail.com>
2021-03-16 14:33:55 +0100

    Loggin Firebase with google

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBj2WWk88IjxOehEb5wiJn0JePAVCFXlxk',
+  authDomain: 'nautilus-b8c31.firebaseapp.com',
+  projectId: 'nautilus-b8c31',
+  storageBucket: 'nautilus-b8c31.appspot.com',
+  messagingSenderId: '148032928019',
+  appId: '1:148032928019:web:71b882a9dffc1e6ed9c2e6'
+};
+
+export default firebaseConfig;
--- frontend/src/redux/actions/actionTypes.js
+++ frontend/src/redux/actions/actionTypes.js
@@ -1,5 +1,7 @@
 const actionTypes = {
-  LOAD_IMMERSIONS: 'LOAD_IMMERSIONS'
+  LOAD_IMMERSIONS: 'LOAD_IMMERSIONS',
+  USER_SING_IN: 'USER_SING_IN',
+  USER_SING_OUT: 'USER_SING_OUT'
 };
 
 export default actionTypes;
