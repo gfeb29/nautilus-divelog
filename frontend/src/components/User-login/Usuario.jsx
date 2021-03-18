994985a3a89ea1686cf37cd7994c6387765144b4
Eloy Galan <gfeb29@gmail.com>
2021-03-18 16:33:12 +0100

    Immersion form with (complete object schema)

--- frontend/src/components/User-login/Usuario.jsx
+++ frontend/src/components/User-login/Usuario.jsx
@@ -3,9 +3,16 @@ import './Usuarios.css';
 
 function Usuarios() {
   const [name, setName] = useState('');
-  const [immersion, setImmersion] = useState('');
+  const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [maxDepth, setMaxDepth] = useState('');
+  const [duration, setDuration] = useState('');
+  const [minimalTemperature, setMinimalTemperature] = useState('');
+  const [immersionNumber, setImmersionNumber] = useState('');
+  const [depthsByTime, setDepthsByTime] = useState('');
   const [list, setList] = useState([]);
-  const [failure, setfailure] = useState(false);
+  const [failure, setFailure] = useState(false);
   const [error, setError] = useState('');
   const [edit, setEdit] = useState(false);
 
@@ -13,25 +20,76 @@ function Usuarios() {
     e.preventDefault();
 
     if (!name.trim()) {
-      setfailure(true);
+      setFailure(true);
       setError('Name is required');
       return;
     }
 
-    if (!immersion.trim()) {
-      setfailure(true);
-      setError('Immersion is required');
+    if (!location.trim()) {
+      setFailure(true);
+      setError('Location is required');
+      return;
+    }
+
+    if (!date.trim()) {
+      setFailure(true);
+      setError('Date is required');
+      return;
+    }
+
+    if (!time.trim()) {
+      setFailure(true);
+      setError('Time is required');
+      return;
+    }
+
+    if (!maxDepth.trim()) {
+      setFailure(true);
+      setError('Max. Depth is required');
+      return;
+    }
+
+    if (!duration.trim()) {
+      setFailure(true);
+      setError('Duration is required');
+      return;
+    }
+
+    if (!minimalTemperature.trim()) {
+      setFailure(true);
+      setError('Minimal Temperature is required');
+      return;
+    }
+
+    if (!immersionNumber.trim()) {
+      setFailure(true);
+      setError('Immersion Number is required');
       return;
     }
 
     const user = {
       name,
-      immersion
+      location,
+      date,
+      time,
+      maxDepth,
+      duration,
+      minimalTemperature,
+      immersionNumber,
+      depthsByTime
+
     };
     setList([...list, user]);
     setName('');
-    setImmersion('');
-    setfailure(false);
+    setLocation('');
+    setDate('');
+    setTime('');
+    setMaxDepth('');
+    setDuration('');
+    setMinimalTemperature('');
+    setImmersionNumber('');
+    setDepthsByTime('');
+    setFailure(false);
   };
 
   // Borrdo "" filtra y devuelve pero no elimina( en el state si que lo elimina)
@@ -43,31 +101,52 @@ function Usuarios() {
 
   const FirstEdit = (object) => {
     setName(object.name);
-    setImmersion(object.immersion);
+    setLocation(object.location);
+    setDate(object.date);
+    setTime(object.time);
+    setMaxDepth(object.maxDepth);
+    setDuration(object.duration);
+    setMinimalTemperature(object.minimalTemperature);
+    setImmersionNumber(object.immersionNumber);
     setEdit(true);
   };
 
   const FinalEdit = (e) => {
     e.preventDefault();
-    // eslint-disable-next-line no-debugger
-    debugger;
-    const edited = list.map((item) => (item.name === name ? { name, immersion } : item));
+
+    const edited = list.map((item) => (item.name === name ? {
+      name,
+      location,
+      date,
+      time,
+      maxDepth,
+      duration,
+      minimalTemperature,
+      immersionNumber,
+      depthsByTime
+    } : item));
     setList(edited);
     setEdit(false);
     setName('');
-    setImmersion('');
+    setLocation('');
+    setDate('');
+    setTime('');
+    setMaxDepth('');
+    setDuration('');
+    setMinimalTemperature('');
+    setImmersionNumber('');
   };
 
   return (
     <div className="users">
       <div className="list">
-        <h2>Listado de Usuarios</h2>
+        <h2>Listado de Immersiones</h2>
         <ul>
           {
               list.map((info) => (
                 <li>
                   {`Name: ${info.name}
-                  Immersion: ${info.immersion}`}
+                  Immersion: ${info.location}`}
                   <button onClick={() => { FirstEdit(info); }} className="" type="button">Editar</button>
                   <button onClick={() => { DeleteImmersion(info.name); }} className="" type="button">Borrar</button>
 
@@ -77,7 +156,7 @@ function Usuarios() {
         </ul>
       </div>
       <div className="form">
-        <h2>Formulario de Usuarios o immersiones</h2>
+        <h2>Formulario de Immersiones</h2>
         {
             failure ? (<span>{error}</span>) : (<span />)
         }
@@ -90,11 +169,60 @@ function Usuarios() {
             value={name}
           />
           <input
-            onChange={(e) => { setImmersion(e.target.value); }}
+            onChange={(e) => { setLocation(e.target.value); }}
             className="form-control"
             type="text"
-            placeholder="immersion"
-            value={immersion}
+            placeholder="Location"
+            value={location}
+          />
+          <input
+            onChange={(e) => { setDate(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="date"
+            value={date}
+          />
+          <input
+            onChange={(e) => { setTime(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="Time"
+            value={time}
+          />
+          <input
+            onChange={(e) => { setMaxDepth(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="Max. depth"
+            value={maxDepth}
+          />
+          <input
+            onChange={(e) => { setDuration(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="Duration"
+            value={duration}
+          />
+          <input
+            onChange={(e) => { setMinimalTemperature(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="Minimal Temperature"
+            value={minimalTemperature}
+          />
+          <input
+            onChange={(e) => { setImmersionNumber(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="Immersion Number"
+            value={immersionNumber}
+          />
+          <input
+            onChange={(e) => { setDepthsByTime(e.target.value); }}
+            className="form-control"
+            type="text"
+            placeholder="Depths by time"
+            value={depthsByTime}
           />
           {
               edit ? (
